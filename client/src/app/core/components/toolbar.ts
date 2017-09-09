9a5bbf92f74cfaa78f60aa8ce97b09b583093fe6
Carlos Ramirez III <carlos.ramirez@toptal.com>
2017-09-09 10:51:18 -0400

    Add icons/buttons to header toolbar

--- client/src/app/core/components/toolbar.ts
+++ client/src/app/core/components/toolbar.ts
@@ -4,8 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-toolbar',
   template: `
     <md-toolbar class="mat-typography" color="primary">
+      <span>Toptal Shop</span>
+
+      <span class="fill-remaining-space"></span>
+
+      <div class="action-list">
+        <button md-icon-button>
+          <md-icon>shopping_cart</md-icon>
+        </button>
+        <button md-icon-button [mdMenuTriggerFor]="appMenu">
+          <md-icon>more_vert</md-icon>
+        </button>
+        <md-menu #appMenu="mdMenu">
+          <button md-menu-item> Register </button>
+          <button md-menu-item> Login </button>
+        </md-menu>
+      </div>
     </md-toolbar>
   `,
-  styles: []
+  styles: [
+    `.fill-remaining-space { flex: 1 1 auto; }`,
+    `.action-list { display: flex; flex-direction: row; align-items: center; }`
+  ]
 })
 export class ToolbarComponent {}
