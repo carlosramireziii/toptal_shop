58d0869e07cb9a84f5c135e1f691528fdf4f9798
Carlos Ramirez III <carlos.ramirez@toptal.com>
2017-09-09 11:03:59 -0400

    Add to cart button on product tiles

--- client/src/app/products/components/product-preview-list.ts
+++ client/src/app/products/components/product-preview-list.ts
@@ -7,11 +7,16 @@ import { Product } from '../models/product';
     <md-grid-list class="mat-typography" cols="3" rowHeight="2:1">
       <md-grid-tile *ngFor="let product of products">
         <img src="http://placehold.it/500x500">
-        <md-grid-tile-header>{{ product.name }}</md-grid-tile-header>
+          <md-grid-tile-footer>
+            <span>{{ product.name }}</span>
+            <button md-icon-button><md-icon>add_shopping_cart</md-icon></button>
+          </md-grid-tile-footer>
       </md-grid-tile>
     </md-grid-list>
   `,
-  styles: []
+  styles: [
+    `.mat-grid-tile-footer { justify-content: space-between; }`,
+  ]
 })
 export class ProductPreviewListComponent {
   @Input() products: Product[];
